4c02d5bbc66c6358e3af30397954dc72c207fffd
af05414 <60174275+af05414@users.noreply.github.com>
2022-03-01 21:09:18 +0500

    Add files via upload

---
+++ Netwroks_hw2_Q4.py
@@ -0,0 +1,74 @@
+import networkx as nx
+import math 
+import statistics
+import matplotlib.pyplot as plt
+
+
+averageDegree = []
+averageClustering = []
+averagePathLenght = []
+
+def average_degree(G):
+    node_degree = []
+    for n in range(1,30):
+        for i in G.nodes():
+            node_degree.append(G.degree(i))
+            average_deg = statistics.mean(node_degree)
+        averageDegree.append(average_deg)
+    return statistics.mean(averageDegree)
+
+def average_clustering(G):
+    for n in range(1,30):
+        clustering = nx.algorithms.cluster.average_clustering(G)
+        averageClustering.append(clustering)
+    return statistics.mean(averageClustering)
+
+def average_path_length(G):
+    for n in range(1,30):
+        path_length = nx.average_shortest_path_length(G)
+        averagePathLenght.append(path_length)    
+    return statistics.mean(averagePathLenght)
+
+def Degree_distribution(G):
+    degrees_dict = {}
+
+    for n in G.nodes():
+        degree = G.degree(n)
+        if degree not in degrees_dict:
+            degrees_dict[degree] = 0
+        degrees_dict[degree] += 1
+    items = sorted(degrees_dict.items())
+
+    plt.plot([k for (k, v) in items], [v for (k, v) in items])
+    
+    plt.ylabel("Number of Nodes")
+    plt.xlabel('Degree of Nodes)')
+    plt.show()
+    return items
+
+
+def inputs1(n,p):
+        G = nx.erdos_renyi_graph(n, p)
+        print( "The average degree of n" , n ,"and p" ,p ,": ", average_degree(G))
+        print( "Average clustering coefficient of n" , n ,"and p" , p ,": " , average_clustering(G))
+        print( "The average path length of n" ,n ,"and p" ,p ,": " , average_path_length(G))
+        print( "Degree Distribution of n" ,n ,"and p" ,p ,": " , Degree_distribution(G))
+            
+def inputs2(n,p):
+        G = nx.erdos_renyi_graph(n, p)
+        print( "The average degree of n" , n ,"and p" ,p ,": ", average_degree(G))
+        print( "Average clustering coefficient of n" , n ,"and p" , p ,": " , average_clustering(G))
+        print( "The average path length of n" ,n ,"and p" ,p ,": " , average_path_length(G))
+        print( "Degree Distribution of n" ,n ,"and p" ,p ,": " , Degree_distribution(G))
+
+def inputs3(n,p):
+        G = nx.erdos_renyi_graph(n, p)
+        print( "The average degree of n" , n ,"and p" ,p ,": ", average_degree(G))
+        print( "Average clustering coefficient of n" , n ,"and p" , p ,": " , average_clustering(G))
+        print( "The average path length of n" ,n ,"and p" ,p ,": " , average_path_length(G))
+        print( "Degree Distribution of n" ,n ,"and p" ,p ,": " , Degree_distribution(G))
+
+
+inputs1(100, 0.2)
+inputs2(200, 0.09)
+inputs3(300, 0.5)
